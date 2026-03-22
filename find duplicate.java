a832e3ff3fe8a0e399b3b379c434d191b55445a3
Ayush-singh351 <chotu2157singh@gmail.com>
2026-03-22 23:37:09 +0530

    Add files via upload

---
+++ find duplicate.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+
+class Solution {
+
+    public int findDuplicate(int arr[]) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int x : arr){
+
+            if(set.contains(x))
+                return x;
+
+            set.add(x);
+        }
+
+        return -1;
+    }
+}
