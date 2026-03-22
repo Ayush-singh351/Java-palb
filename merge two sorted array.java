3d0ea81e59afcd4270187e3980d7d8721051f595
Ayush-singh351 <chotu2157singh@gmail.com>
2026-03-22 23:38:50 +0530

    Add files via upload

---
+++ merge two sorted array.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    public void merge(int a[], int b[]) {
+
+        int n = a.length;
+        int m = b.length;
+
+        for(int i=0;i<n;i++){
+
+            if(a[i] > b[0]){
+
+                int temp = a[i];
+                a[i] = b[0];
+                b[0] = temp;
+
+                int first = b[0];
+                int k;
+
+                for(k=1;k<m && b[k] < first;k++){
+                    b[k-1] = b[k];
+                }
+
+                b[k-1] = first;
+            }
+        }
+    }
+}
