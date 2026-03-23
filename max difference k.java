9f9da7c45ee22de7170086e9e4597cf4f4c50b72
Ayush-singh351 <chotu2157singh@gmail.com>
2026-03-23 18:03:13 +0530

    Add files via upload

---
+++ max difference k.java
@@ -0,0 +1,19 @@
+class Solution {
+
+    public int diff(int arr[]){
+
+        int max=arr[0];
+        int min=arr[0];
+
+        for(int i=0;i<arr.length;i++){
+
+            if(arr[i]>max)
+                max=arr[i];
+
+            if(arr[i]<min)
+                min=arr[i];
+        }
+
+        return max-min;
+    }
+}
