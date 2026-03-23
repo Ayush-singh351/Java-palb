bc8fb7eea76f884833af7015323a685dd5b48a9b
Ayush-singh351 <chotu2157singh@gmail.com>
2026-03-23 18:01:26 +0530

    Add files via upload

---
+++ tug of war.java
@@ -0,0 +1,15 @@
+class Solution {
+
+    public boolean check(int arr[]){
+
+        int s=0;
+
+        for(int i=0;i<arr.length;i++)
+            s+=arr[i];
+
+        if(s%2==0)
+            return true;
+
+        return false;
+    }
+}
