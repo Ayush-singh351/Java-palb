b0b09055a02c9982e0d3193f1876bf6d5d58a360
Ayush-singh351 <chotu2157singh@gmail.com>
2026-03-23 18:12:51 +0530

    Add files via upload

---
+++ valid parentheses add.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    public int add(String s){
+
+        int open=0;
+        int add=0;
+
+        for(int i=0;i<s.length();i++){
+
+            if(s.charAt(i)=='(')
+                open++;
+
+            else{
+
+                if(open>0)
+                    open--;
+                else
+                    add++;
+            }
+        }
+
+        return open+add;
+    }
+}
